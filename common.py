2ee886bcec6080afe724973ae731111b71ed71fe
Bluetooth503 <lanenlai.abcd@gmail.com>
2025-01-12 20:57:49 +0800

    20250112,更新

---
+++ common.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+from sqlalchemy import create_engine
+import psycopg2
+from datetime import datetime, date, timedelta
+import time
+from loguru import logger
+import warnings
+warnings.filterwarnings("ignore") # 屏蔽jupyter的告警显示
+import pandas as pd
+pd.set_option('display.float_format',lambda x:'%.4f' % x)
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False    # 用来正常显示负号
+import configparser
+
+
+def convert_to_baostock_code(ts_code: str) -> str:
+    """将 tushare 格式的代码转换为 baostock 格式"""
+    code, market = ts_code.split('.')
+    if market == 'SZ':
+        return f"sz.{code}"
+    return f"sh.{code}"
+
+def convert_to_tushare_code(baostock_code: str) -> str:
+    """将 baostock 格式的代码转换为 tushare 格式"""
+    market, code = baostock_code.split('.')
+    market = market.upper()
+    return f"{code}.{market}"
+
+def load_config():
+    """加载配置文件"""
+    config = configparser.ConfigParser()
+    config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    if not os.path.exists(config_path):
+        raise FileNotFoundError("配置文件 'config.ini' 不存在！")
+    
+    config.read(config_path, encoding='utf-8')
+    return config
+
